3fd36f3c618f8033c75c8d139a13c954df0e155d
Yaheli Levit <yaheli.levit@post.runi.ac.il>
2026-02-26 15:20:00 +0200

    Final touches

---
+++ MAZEFOLDER/BlueTank.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="BlueTank" tilewidth="31" tileheight="35" tilecount="1" columns="1">
+ <image source="../imagefolder/tank_blue.png" width="31" height="35"/>
+</tileset>
---
+++ MAZEFOLDER/white.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="white" tilewidth="16" tileheight="16" tilecount="2" columns="1">
+ <image source="../imagefolder/white.png" width="31" height="35"/>
+</tileset>
